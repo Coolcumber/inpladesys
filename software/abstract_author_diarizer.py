eadc50d1b4667d4399814c69cf74cd847282510c
ivan.grubisic.mai@gmail.com <ivan.grubisic.mai@gmail.com>
2017-03-30 17:39:00 +0200

    Changed not much

--- software/abstract_author_diarizer.py
+++ software/abstract_author_diarizer.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-
 from datatypes import *
 from typing import List, Tuple
 
--- software/abstract_feature_extractor.py
+++ software/abstract_feature_extractor.py
@@ -9,6 +9,17 @@ def AbstractFeatureExtractor(ABC):  # TODO
     def train(self, dataset: Dataset):
         pass
 
+    def set_document(self, document):
+        """
+        As features of a part of a ocument might depend on the surroundong text,
+        a feature extractor might like to heve the whole document available.
+        """
+        self.document = document
+
     @abstractmethod
-    def get_features(document, segment) -> np.ndarray:
+    def get_features(self, segments) -> np.ndarray:
+        """
+        Returns an array of feature vectors for a list of segments or a single
+        segment.
+        """
         pass
