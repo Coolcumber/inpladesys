f249fe4732e0dd25f49252533fd9caa6fc3a9f70
Milan <milan.pavlovic711@gmail.com>
2017-05-05 16:58:42 +0200

    General TODO list for the end of project created.

---
+++ software/inpladesys/TODOs.py
@@ -0,0 +1,17 @@
+# TODO allow SimpleFixedAuthorDiarizer to adjust number of authors in BASELINE tests
+# TODO implement interfacce for models
+# TODO fix warnings in training
+# TODO add new features (new "iteration"):
+#   - type-token ratio (vocabulary richness)
+#   - word n-grams ?
+#   - char n-grams of POS tags
+#   - chunks (phrases)
+#   - synonyms ?
+# TODO try other algorithms
+# TODO add writing of test results to a file
+# TODO experiment with params (what combs ?)
+# TODO solve task a
+# TODO finish neural net
+# TODO implement elbow method (or some other ?) for task c
+# TODO implement t-test for all tasks
+
