9447ff59eb073b7bbea6dcdcb0fd0a9604f980b5
Milan <milan.pavlovic711@gmail.com>
2017-05-20 00:18:29 +0200

    Custom dataset splitter moved to misc.py.

--- software/inpladesys/models/misc/misc.py
+++ software/inpladesys/models/misc/misc.py
@@ -1,6 +1,8 @@
 from inpladesys.datatypes import Segment, Segmentation
 from typing import List
 import numpy as np
+from inpladesys.datatypes.dataset import Dataset
+from sklearn.model_selection import train_test_split
 
 
 def generate_segmentation(preprocessed_documents: List[List[tuple]], documents_features: List[np.ndarray],
@@ -23,3 +25,29 @@ def generate_segmentation(preprocessed_documents: List[List[tuple]], documents_f
                                           max_repairable_error=60,
                                           document_length=len(documents[i])))
     return segmentations
+
+
+def custom_train_test_split(preprocessed_documents: List[List[tuple]], documents_features: List[np.ndarray],
+                            dataset: Dataset, train_size, random_state):
+
+    # indices of every document
+    indices_of_docs = [i for i in range(len(preprocessed_documents))]
+
+    i_train, i_test = train_test_split(indices_of_docs, train_size=train_size, random_state=random_state)
+
+    prep_docs_train = [preprocessed_documents[i] for i in i_train]
+    prep_docs_test = [preprocessed_documents[i] for i in i_test]
+
+    doc_features_train = [documents_features[i] for i in i_train]
+    doc_features_test = [documents_features[i] for i in i_test]
+
+    author_counts_train = [dataset.segmentations[i].author_count for i in i_train]
+    author_counts_test = [dataset.segmentations[i].author_count for i in i_test]
+
+    documents_train = [dataset.documents[i] for i in i_train]
+    documents_test = [dataset.documents[i] for i in i_test]
+
+    return prep_docs_train, prep_docs_test, \
+           doc_features_train, doc_features_test, \
+           author_counts_train, author_counts_test, \
+           documents_train, documents_test
