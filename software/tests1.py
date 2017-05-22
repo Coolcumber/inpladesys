d6dfa84ebc88eb93834647fb2120e39b97867b67
Milan <milan.pavlovic711@gmail.com>
2017-05-23 01:51:25 +0200

    Baseline results recalculated.

--- software/tests1.py
+++ software/tests1.py
@@ -19,7 +19,7 @@ dataset_dirs = [
     "../data/pan16-author-diarization-training-dataset-problem-c-2016-02-16"
 ]
 print("Loading dataset...")
-dataset_index = 0
+dataset_index = 2
 dataset = Pan16DatasetLoader(dataset_dirs[dataset_index]).load_dataset()
 
 models = [
@@ -63,27 +63,53 @@ if False:
 print("Testing on the dataset...")
 modelsToScores = dict(
     (m.name, np.array([0, 0, 0], dtype=float)) for m in models)
+
+averagedModelsToScores = dict(
+    (m.name, np.array([0, 0, 0], dtype=float)) for m in models)
+
+
 for model in models:
     m = model()
-    m.fit(dataset.documents, dataset.segmentations)
-    for d, truth in zip(dataset.documents, dataset.segmentations):
-        if isinstance(m, SimpleFixedAuthorDiarizer):
-            if dataset_index != 2:  # tasks a and b
-                m.author_count = truth.author_count
-            else:  # task c
-                m.choose_author_count(d)
-        pred = m.predict(d)
-        if dataset_index == 0:  # task a
-            #micro = MicroScorer(truth, pred)  # use Micro AND Macro Scorer for task a!!
-            macro = MacroScorer(truth, pred)  # use Micro AND Macro Scorer for task a!!
-            modelsToScores[model.name] += np.array([macro.precision(), macro.recall(), macro.f1_score()])
+    iterations = 1
+    if isinstance(m, DummyStochasticAuthorDiarizer) or \
+            (isinstance(m, SimpleFixedAuthorDiarizer) and dataset_index == 2):
+        iterations = 50
 
-        else:  # tasks b and c
-            scorer = BCubedScorer(truth, pred)
-            modelsToScores[model.name] += np.array([scorer.precision(),
-                                                    scorer.recall(), scorer.f1_score()])
+    for i in range(iterations):
+        print('Iteration {}/{}: {}'.format(i+1, iterations, model.name))
+        m.fit(dataset.documents, dataset.segmentations)
+        for d, truth in zip(dataset.documents, dataset.segmentations):
+            if isinstance(m, SimpleFixedAuthorDiarizer):
+                if dataset_index != 2:  # tasks a and b
+                    m.author_count = truth.author_count
+                else:  # task c
+                    m.choose_author_count(d)
+            pred = m.predict(d)
+            if dataset_index == 0:  # task a
+                #micro = MicroScorer(truth, pred)  # use Micro AND Macro Scorer for task a!!
+                macro = MicroScorer(truth, pred)  # use Micro AND Macro Scorer for task a!!
+                modelsToScores[model.name] += np.array([macro.precision(), macro.recall(), macro.f1_score()])
 
+            else:  # tasks b and c
+                scorer = BCubedScorer(truth, pred)
+                modelsToScores[model.name] += np.array([scorer.precision(),
+                                                        scorer.recall(), scorer.f1_score()])
+
+        # calculate average on dataset and add to new array
+        modelsToScores[model.name] /= len(dataset.documents)
+        averagedModelsToScores[model.name] += modelsToScores[model.name]
+        modelsToScores[model.name] = np.array([0, 0, 0], dtype=float)
+
+    # calculate average on iterations
+    averagedModelsToScores[model.name] /= iterations
+    print()
+
+print('\nFINAL RESULTS:')
 for model in models:
-    modelsToScores[model.name] /= len(dataset.documents)
     print(model.name)
-    print(modelsToScores[model.name])
+    print(averagedModelsToScores[model.name])
+    print()
+
+ #   modelsToScores[model.name] /= len(dataset.documents)
+  #  print(model.name)
+   # print(modelsToScores[model.name])
